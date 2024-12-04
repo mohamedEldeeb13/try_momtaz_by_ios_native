b668965e934f02625b2a82b81240d3dc4670787b
mohamedEldeeb13 <mooel744@gmail.com>
2024-12-05 01:14:54 +0200

    intail commit

---
+++ Momtaz-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Momtaz-IOS
+//
+//  Created by Mohamed Abd Elhakam on 05/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
